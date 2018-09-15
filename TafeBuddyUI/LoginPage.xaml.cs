e1d2b4922863cecf52005b25a1c496335efecbcb
Ye Lin <erenst2002@hotmail.com>
2018-09-15 09:37:36 +0930

    Add new page "MainPageAfterLogin"
    Add new page "MainPageAfterLogin"
    Please put the links of your page in it

--- TafeBuddyUI/LoginPage.xaml.cs
+++ TafeBuddyUI/LoginPage.xaml.cs
@@ -13,30 +13,28 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace TafeBuddyUI
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class LoginPage : Page
     {
-        public MainPage()
+        public LoginPage()
         {
             this.InitializeComponent();
         }
 
-        private void CFIButton_Click(object sender, RoutedEventArgs e)
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
         {
-            
+            this.Frame.Navigate(typeof(TafeBuddyUI.MainPage));
         }
 
-        private void LogInbutton_Click(object sender, RoutedEventArgs e)
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
         {
-            this.Frame.Navigate(typeof(TafeBuddyUI.LoginPage));
+            this.Frame.Navigate(typeof(TafeBuddyUI.MainPageAfterLogin));
         }
     }
-
 }
-
--- TafeBuddyUI/MainPageAfterLogin.xaml.cs
+++ TafeBuddyUI/MainPageAfterLogin.xaml.cs
@@ -13,30 +13,23 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace TafeBuddyUI
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class MainPageAfterLogin : Page
     {
-        public MainPage()
+        public MainPageAfterLogin()
         {
             this.InitializeComponent();
         }
 
-        private void CFIButton_Click(object sender, RoutedEventArgs e)
+        private void LogOutbutton_Click(object sender, RoutedEventArgs e)
         {
-            
-        }
-
-        private void LogInbutton_Click(object sender, RoutedEventArgs e)
-        {
-            this.Frame.Navigate(typeof(TafeBuddyUI.LoginPage));
+            this.Frame.Navigate(typeof(TafeBuddyUI.MainPage));
         }
     }
-
 }
-
